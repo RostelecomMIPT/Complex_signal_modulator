34d35daa66322df9c54d71c13930871a31b56428
federalos <f3d3ral@yandex.ru>
2016-10-13 18:06:07 +0400

    Ошибка. предыдущий шаг неверен. с шумом не добился синхронизации частотной

--- main.m
+++ main.m
@@ -9,7 +9,7 @@ Nsk = 16;
 Nfft = 1024;
 Nc = 100;
 w = 0.1;
-NumbOfSymbol = 3;
+NumbOfSymbol = 2;
 SNR = 0;
 
 %אכדמנטעל
@@ -20,13 +20,13 @@ MedSignalInF = Mapper(Bits, Nsk, Nfft);
 SignalTs = AddTs (Signal, Nfft);
 IQ_Ts_Shift = Shift( SignalTs, w, Nfft );
 IQ_Ts_Shift_Noise = awgn(IQ_Ts_Shift,SNR,'measured');
-% plot(abs(IQ_Ts_Shift) - abs(IQ_Ts_Shift_Noise));
-% hold on;
-% plot(abs(IQ_Ts_Shift));
-% plot(abs(IQ_Ts_Shift_Noise));
-sdv = FindOfPhase(IQ_Ts_Shift, Nfft);
-IQ_Ts_Unshifted = Shift(IQ_Ts_Shift,sdv,Nfft);
-for k = 1 : NumbOfSymbol
-    scatterplot(fft(IQ_Ts_Unshifted(Nfft/8+ 1 + (k-1)*(Nfft+Nfft/8):k*(Nfft+Nfft/8))));
-end
+plot(abs(IQ_Ts_Shift) - abs(IQ_Ts_Shift_Noise));
+hold on;
+plot(abs(IQ_Ts_Shift));
+plot(abs(IQ_Ts_Shift_Noise));
+sdv = FindOfPhase(IQ_Ts_Shift_Noise, Nfft);
+IQ_Ts_Unshifted = Shift(IQ_Ts_Shift_Noise, sdv, Nfft);
+% for k = 1 : NumbOfSymbol
+%     scatterplot(fft(IQ_Ts_Unshifted(Nfft/8+ 1 + (k-1)*(Nfft+Nfft/8):k*(Nfft+Nfft/8))));
+% end
 z=0;
