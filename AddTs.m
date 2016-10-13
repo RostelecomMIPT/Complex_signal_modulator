6d6f29e6cf9e2b4ccb1e49abb79918eb21cf3c03
federalos <f3d3ral@yandex.ru>
2016-10-13 16:54:34 +0400

    Ранее была ошибка. сейчас исправлена позиция смещения и вставки защитного интервала. Но СДВ считает неверно

--- AddTs.m
+++ AddTs.m
@@ -1,10 +1,10 @@
-function [ IQ_Ts ] = AddTs( IQ, Nfft )
-    IQ_Ts = [ IQ(Nfft-Nfft/8 + 1 : Nfft) IQ(1:Nfft) ];
-    if length(IQ)/Nfft > 1
-        for k = 2 : length(IQ)/Nfft
-            IQ_Ts = [ IQ_Ts ...
-                      IQ( (k*Nfft - Nfft/8 + 1) : k*Nfft)...
-                      IQ( (k-1)*Nfft + 1 : k*Nfft) ];
+function [ Signal_Ts ] = AddTs( Signal, Nfft )
+    Signal_Ts = [ Signal(Nfft-Nfft/8 + 1 : Nfft) Signal(1:Nfft) ];
+    if length(Signal)/Nfft > 1
+        for k = 2 : length(Signal)/Nfft
+            Signal_Ts = [ Signal_Ts ...
+                      Signal( (k*Nfft - Nfft/8 + 1) : k*Nfft)...
+                      Signal( (k-1)*Nfft + 1 : k*Nfft) ];
         end
     end
 end
