59e869547b9316df13892e401354c9c929a75c1f
federalos <f3d3ral@yandex.ru>
2016-10-14 15:09:29 +0400

    Перевёл цикл(закомментированный) в матлабовский стиль. Раотает быстрее

--- TimeSync.m
+++ TimeSync.m
@@ -1,17 +1,19 @@
 function [ AutoCorr ] = TimeSync( IQ_Ts_Unshifted, Nfft )
-    MedIQ = IQ_Ts_Unshifted;
-    for k = 1 : length(MedIQ) - Nfft/8 - Nfft
-        Up = 0;
-        Down1 = 0;
-        Down2 = 0;
-        for l = 1 : Nfft/16
-            Up = Up + MedIQ(l + k - 1)*...
-                    (MedIQ(l + k - 1 + Nfft))';
-            Down1 = Down1 + MedIQ(l + k - 1)*...
-                        (MedIQ(l + k - 1))';
-            Down2 = Down2 + MedIQ(l + k - 1 + Nfft)*...
-                        (MedIQ(l + k - 1 + Nfft))';
-        end
+    for k = 1 : length(IQ_Ts_Unshifted) - Nfft/16 - Nfft
+%         for l = 1 : Nfft/16
+%             Up = Up + IQ_Ts_Unshifted(l + k - 1)*...
+%                     (IQ_Ts_Unshifted(l + k - 1 + Nfft))';
+%             Down1 = Down1 + IQ_Ts_Unshifted(l + k - 1)*...
+%                         (IQ_Ts_Unshifted(l + k - 1))';
+%             Down2 = Down2 + IQ_Ts_Unshifted(l + k - 1 + Nfft)*...
+%                         (IQ_Ts_Unshifted(l + k - 1 + Nfft))';
+%         end
+        Up = sum (IQ_Ts_Unshifted(k : k + Nfft/16).*...
+            conj(IQ_Ts_Unshifted(k + Nfft : k + Nfft + Nfft/16)));
+        Down1 = sum(IQ_Ts_Unshifted(k : k + Nfft/16).*...
+            conj(IQ_Ts_Unshifted(k : k + Nfft/16)));
+        Down2 = sum(IQ_Ts_Unshifted(k + Nfft : k + Nfft + Nfft/16).*...
+            conj(IQ_Ts_Unshifted(k + Nfft : k + Nfft + Nfft/16)));
         AutoCorr(k) = Up/sqrt(Down1*Down2);
     end
     plot(abs(AutoCorr))
