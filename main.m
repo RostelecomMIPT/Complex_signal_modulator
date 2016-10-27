30ea8532f2c54ddaabbeba0f7fe6bf8c4bbb5448
federalos <f3d3ral@yandex.ru>
2016-10-27 20:13:26 +0400

    странно и не работает. Смщение закономерное

--- main.m
+++ main.m
@@ -8,7 +8,7 @@ Register = [1 0 0 1 0 1 0 1 0 0 0 0 0 0 0 ];
 Nsk = 16;
 Nfft = 1024;
 Nc = 500;
-w = 0.1;
+w = 0;
 NumbOfSymbol = 5;
 SNR = 500;
 LevelOfIncreasing = 3;
@@ -31,17 +31,17 @@ IQ_Ts_Shift_Noise = awgn(IQ_Ts_Shift, SNR, 'measured');
 % Выше - модуль с модулятором
 % Ниже - модуль с демодулятором. Сигнал приходит на приёмник
 
-IQ_Ts_Shift_Noise(1:500) = [];
+IQ_Ts_Shift_Noise(1:64) = [];
 %Грубый поиск середины защитного интервала
-[ AbsAutoCorr, AutoCorr, PositionTs1 ] = FuncCorrelation(...
-        IQ_Ts_Shift_Noise, Nfft, LevelOfIncreasing );
+% [ AbsAutoCorr, AutoCorr, PositionTs1 ] = FuncCorrelation(...
+%         IQ_Ts_Shift_Noise, Nfft, LevelOfIncreasing );
 %Частотная синхронизация
-PhaseFreq =  FindOfPhase(AutoCorr, Nfft, PositionTs1);
-IQ_Ts_Unshifted = Shift(IQ_Ts_Shift_Noise, PhaseFreq, Fi, Nfft);
+% PhaseFreq =  FindOfPhase(AutoCorr, Nfft, PositionTs1);
+IQ_Ts_Unshifted = Shift(IQ_Ts_Shift_Noise, 0, Fi, Nfft);
 w = 0;
 %далее поиск точной позиции ОФДМ-символа
 IQ_Ts_Unsifted_TimeSync = ...
-    FuncTs(IQ_Ts_Unshifted, Nfft, LevelOfIncreasing , Index_Pilot);
+    FuncTs( IQ_Ts_Unshifted, Nfft, LevelOfIncreasing , Index_Pilot );
 % for k = 1 : NumbOfSymbol - 1
 %     scatterplot(fft(IQ_Ts_Unshifted(Nfft/8+ 1 + (k-1)*(Nfft+Nfft/8):k*(Nfft+Nfft/8))));
 % end
